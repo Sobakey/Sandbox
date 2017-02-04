cb3c3c131e97ec10f7822ab90a37bb60ff9bcc0e
Sobakey <kail.74rus@gmail.com>
2017-02-04 12:46:25 +0300

    1
    1

--- Sandbox2d/Assets/Scripts/PolygonGenerator.cs
+++ Sandbox2d/Assets/Scripts/PolygonGenerator.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PolygonGenerator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public List<Vector3> newVertices = new List<Vector3>();
+    public List<int> newTriangles = new List<int>();
+    public List<Vector2> newUV = new List<Vector2>();
+
+    private Mesh mesh;
+
+    // Use this for initialization
+    void Start () {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+
+        newVertices.Add(new Vector3(x, y, z));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x + 1, y - 1, z));
+        newVertices.Add(new Vector3(x, y - 1, z));
+
+        newTriangles.Add(0);
+        newTriangles.Add(1);
+        newTriangles.Add(3);
+        newTriangles.Add(1);
+        newTriangles.Add(2);
+        newTriangles.Add(3);
+
+        mesh.Clear();
+        mesh.vertices = newVertices.ToArray();
+        mesh.triangles = newTriangles.ToArray();
+        MeshUtility.Optimize(mesh);
+        
+        mesh.RecalculateNormals();
+    }
 	
 	// Update is called once per frame
 	void Update () {
