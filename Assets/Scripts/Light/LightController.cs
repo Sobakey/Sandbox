d863db584fd515702f2e39e205008c2e6d8b5be5
daxaker <itjustsoap@gmail.com>
2017-02-19 15:18:45 +0300

    Light system files added

---
+++ Assets/Scripts/Light/LightController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LightController : MonoBehaviour {
+
+	// Use this for initialization
+	MeshRenderer renderer;
+	public float emission = 1;
+	public float obstacle = 20;
+	void Start () {
+		renderer = gameObject.GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.SetFloat("_EmissionColorMul", emission);
+		renderer.material.SetFloat("_ObstacleMul", obstacle);
+	}
+}
